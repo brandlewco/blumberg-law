0711e3022df20425b3c84c98e3be383e861c8103
Stackbit Code Editor <projects@stackbit.com>
2022-02-05 22:50:45 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -233,7 +233,7 @@ function listOfLinks(links, inMobileMenu = false) {
                                 ))}
                             </ul>
                         }
-                    </>
+                    <div/>
                 )}
                 {linkChild.length == 0 && (
                         <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
