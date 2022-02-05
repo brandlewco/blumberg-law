a45713536e908c799bdc3e4d62880f1e7e6b1e56
Stackbit Code Editor <projects@stackbit.com>
2022-02-05 20:23:26 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -104,7 +104,7 @@ function heroBody(props) {
         <div>
             {props.title && (
                 <Markdown
-                    options={{ forceBlock: true, forceWrapper: true }}
+                    options={{ forceWrapper: true }}
                      className={classNames('h1', styles.title ? mapStyles(styles.title) : null)} data-sb-field-path=".title">
                     {props.title}
                 </Markdown>
