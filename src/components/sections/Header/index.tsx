cfd8e6d773d23edc98172f091485653ec060c26f
Stackbit Code Editor <projects@stackbit.com>
2022-02-05 23:05:01 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -215,7 +215,7 @@ function listOfLinks(links, inMobileMenu = false) {
         // const defaultStyle = link.type === 'Link' ? 'link' : 'secondary';
         // const style = link.style || defaultStyle;
         const linkChild = link.child || [];
-        return (
+        return (<>
                 {linkChild.length > 0 && (
                                 <li key={index}>
 
