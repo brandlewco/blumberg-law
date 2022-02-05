d4395c1f8674482db658da00acdf989d8be95e2d
Stackbit Code Editor <projects@stackbit.com>
2022-02-05 23:26:47 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -222,7 +222,7 @@ function listOfLinks(links, inMobileMenu = false) {
                         {/* <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={handleClick}> */}
                         <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
                         {/* </div> */}
-                        <ul className={classNames('absolute','top-5','flex','flex-col', open ? 'block' : 'hidden')}>
+                        <ul className={classNames(secondaryColors,'absolute','top-5','flex','flex-col', open ? 'block' : 'hidden')}>
                             {linkChild.map((link, index) => (
                                 <Action {...link} className={""} data-sb-field-path={`.${index}`} />
                             ))}
