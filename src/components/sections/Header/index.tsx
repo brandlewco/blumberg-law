bb32f7d06cd6977cfe360a8849838c7469402d87
Stackbit Code Editor <projects@stackbit.com>
2022-02-05 23:27:12 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -196,7 +196,7 @@ function siteLogoLink(props) {
 
 function listOfLinks(links, inMobileMenu = false) {
     return links.map((link, index) => {
-        const secondaryColors = props.secondaryColors || 'colors-d';
+        const secondaryColors =  'colors-d';
         const [open, setOpen] = useState(false);
         const handleClick = () => { setOpen(!open) };
         const onMouseEnter = () => {
