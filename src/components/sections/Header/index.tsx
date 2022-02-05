f23d5cc0a10f75e8d823cb84e2ab6b51dc2cfc35
Stackbit Code Editor <projects@stackbit.com>
2022-02-05 22:03:19 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -212,6 +212,8 @@ function listOfLinks(links, inMobileMenu = false) {
                         ))}
                     </Menu.Items>
                     </ul>
+                    </>
+            )}
             </li>
             </Menu>
         );
