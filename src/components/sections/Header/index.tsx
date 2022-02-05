26fad23cc6f0cb918b0e15d2c721ec10234ab608
Stackbit Code Editor <projects@stackbit.com>
2022-02-05 22:51:12 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -225,7 +225,7 @@ function listOfLinks(links, inMobileMenu = false) {
             <li key={index}>
                 {linkChild.length > 0 && (
                     <>
-                        <Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
+                        <div></div><Action {...link} className={classNames(inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
                         {open &&
                             <ul>
                                 {linkChild.map((link, index) => (
