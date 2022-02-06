1987dbefb8fcafdfea216cb4a58cd90b74e35bc3
Stackbit Code Editor <projects@stackbit.com>
2022-02-06 20:04:11 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -194,7 +194,7 @@ function siteLogoLink(props) {
 //     ));
 // }
 
-function listOfLinks(props, links, inMobileMenu = false) {
+function listOfLinks(links, inMobileMenu = false) {
     return links.map((link, index) => {
         const secondaryColors =  'colors-d';
         const paddings = link.styles?.self || {};
