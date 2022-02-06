1c00f5adeb29129416bbe91e01e7080ea1943477
Stackbit Code Editor <projects@stackbit.com>
2022-02-06 20:43:38 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -69,7 +69,7 @@ function headerVariantB(props) {
     const primaryLinks = props.primaryLinks || [];
     const secondaryLinks = props.secondaryLinks || [];
     return (
-        <div className="flex items-center justify-between relative">
+        <div className="flex items-center justify-around relative">
             {(props.logo || (props.title && props.isTitleVisible)) && <div className="mr-8">{siteLogoLink(props)}</div>}
             {primaryLinks.length > 0 && (
                 <ul
