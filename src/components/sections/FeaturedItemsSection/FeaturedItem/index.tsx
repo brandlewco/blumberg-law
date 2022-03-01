670a04ed562b010d6741a772eaa645de72f8e9cb
Stackbit Code Editor <projects@stackbit.com>
2022-03-01 01:31:24 +0000

    index.tsx: changed

--- src/components/sections/FeaturedItemsSection/FeaturedItem/index.tsx
+++ src/components/sections/FeaturedItemsSection/FeaturedItem/index.tsx
@@ -20,7 +20,7 @@ export default function FeaturedItem(props) {
                 'sb-component-block',
                 'sb-component-item',
                 props.enableHover ? 'sb-component-item-hover' : null,
-                mapStyles(styles.self),mapStyles({ alignItems: sectionAlignItems }),
+                mapStyles(styles.self), mapStyles({ alignItems: sectionAlignItems }),
             )}
             style={{
                 borderWidth: itemBorderWidth ? `${itemBorderWidth}px` : undefined
