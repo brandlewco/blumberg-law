765a7795c25042c06ae4331f47fb95972da82b0b
Stackbit Code Editor <projects@stackbit.com>
2022-03-01 01:49:59 +0000

    index.tsx: changed

--- src/components/sections/FeaturedItemsSection/FeaturedItem/index.tsx
+++ src/components/sections/FeaturedItemsSection/FeaturedItem/index.tsx
@@ -11,7 +11,7 @@ export default function FeaturedItem(props) {
     const styles = props.styles || {};
     const itemBorderWidth = styles.self?.borderWidth ? styles.self?.borderWidth : 0;
     const sectionAlignItems = props.alignItems || 'center';
-        const sectionJustifyContent = sectionStyles.justifyContent || 'center';
+    const sectionJustifyContent = sectionStyles.justifyContent || 'center';
 
 
     return (
