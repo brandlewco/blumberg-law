d9fd631eb048838937521664c45ec877c4498534
Stackbit Code Editor <projects@stackbit.com>
2022-03-16 04:35:30 +0000

    index.tsx: changed

--- src/components/atoms/Action/index.tsx
+++ src/components/atoms/Action/index.tsx
@@ -100,7 +100,7 @@ export default function Action(props) {
             data-sb-field-path={annotations.join(' ').trim()}
         >
             <Markdown
-                options={{ forceWrapper: true }}
+                options={{ }}
                 className={""} data-sb-field-path=".label">
                 {label}
             </Markdown>
