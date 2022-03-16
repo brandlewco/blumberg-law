95c8727c7f7a25f91d679e057a9923c999319496
Stackbit Code Editor <projects@stackbit.com>
2022-03-16 01:57:33 +0000

    index.tsx: changed

--- src/components/atoms/Action/index.tsx
+++ src/components/atoms/Action/index.tsx
@@ -23,7 +23,7 @@ import Send from '../../svgs/send';
 import Twitter from '../../svgs/twitter';
 import Vimeo from '../../svgs/vimeo';
 import YouTube from '../../svgs/youtube';
-import ChildCustody from '../../svgs/child-custody';
+import ChildCustody from '../../svgs/a-child-custody';
 
 
 const iconMap = {
