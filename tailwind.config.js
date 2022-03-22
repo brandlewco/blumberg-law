85b789a4efd972f4b6c6c13744126192a9c0d246
Stackbit Code Editor <projects@stackbit.com>
2022-03-22 02:07:57 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
                 '9/16': '56.25%'
             },
             fontFamily: {
-                fontPrimary: ['Cinzel, 'serif'],
+                fontPrimary: ['Cinzel', 'serif'],
                 fontSecondary: ['Roboto Mono', 'monospace']
             }
         }
