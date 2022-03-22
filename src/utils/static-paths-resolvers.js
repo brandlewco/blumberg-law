b8a22daad67aad5a42b66993b5e1bc3229dcf66e
brandlew <hello@brandlew.co>
2022-03-21 21:29:59 -0700

    cases end

--- src/utils/static-paths-resolvers.js
+++ src/utils/static-paths-resolvers.js
@@ -28,10 +28,5 @@ const StaticPathsResolvers = {
         const categoryId = page.__metadata?.id;
         const categoryPosts = getAllAuthorPostsSorted(objects, categoryId);
         return generatePagedPathsForPage(page, categoryPosts, 10);
-    },
-    Cases: (page, objects) => {
-        const categoryId = page.__metadata?.id;
-        const categoryPosts = getAllAuthorPostsSorted(objects, categoryId);
-        return generatePagedPathsForPage(page, categoryPosts, 10);
     }
 };
