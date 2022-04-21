ed4ef66915ba18bb9299b8bc0a83476fe8f649b6
brandlew <hello@brandlew.co>
2022-04-20 19:42:55 -0700

    redirects file

--- src/components/layouts/PostLayout/index.tsx
+++ src/components/layouts/PostLayout/index.tsx
@@ -19,10 +19,10 @@ export default function PostLayout(props) {
     return (
         <BaseLayout page={page} site={site}>
             <main id="main" className="sb-layout sb-post-layout">
-            {page.media && <div className="w-full ">{postMedia(page.media)}</div>}
-                <article className={classNames(colors, 'px-4', 'sm:px-8', 'py-14', 'lg:py-20')}>
-                    <div className="max-w-7xl mx-auto">
-                        <header className="max-w-5xl mx-auto mb-12 text-left">
+                <article className={classNames(colors, 'px-4', 'sm:px-8', 'py-14', 'lg:py-12')}>
+                    <div className="max-w-5xl mx-auto">
+                        {page.media && <div className="w-full">{postMedia(page.media)}</div>}
+                        <header className="max-w-5xl mx-auto my-12 text-left">
                             {page.title && <h1 data-sb-field-path="title">{page.title}</h1>}
                             <div className="text-lg mt-6">
                                 <span>
@@ -34,7 +34,7 @@ export default function PostLayout(props) {
                             </div>
                         </header>
                         {page.markdown_content && (
-                            <Markdown options={{ forceBlock: true }} className="sb-markdown max-w-screen-md mx-auto markdown-image" data-sb-field-path="markdown_content">
+                            <Markdown options={{ forceBlock: true }} className="sb-markdown max-w-5-xl mx-auto markdown-image" data-sb-field-path="markdown_content">
                                 {page.markdown_content}
                             </Markdown>
                         )}
@@ -65,7 +65,7 @@ function postMedia(media) {
     if (!Media) {
         throw new Error(`no component matching the hero section media type: ${mediaType}`);
     }
-    return <Media {...media} className={classNames({ 'object-cover h-72 w-full': mediaType === 'ImageBlock' })} data-sb-field-path="media" />;
+    return <Media {...media} className={classNames({ 'object-cover w-full h-80': mediaType === 'ImageBlock' })} data-sb-field-path="media" />;
 }
 
 function PostAttribution({ post }) {
@@ -104,8 +104,8 @@ function postAuthor(author) {
             {children}
         </Link>
     ) : (
-            <span data-sb-field-path="author">{children}</span>
-        );
+        <span data-sb-field-path="author">{children}</span>
+    );
 }
 
 function postCategory(category) {
