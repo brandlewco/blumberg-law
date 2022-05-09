96c84fc266c0839945ea7152a02b87297756efd1
brandlew <hello@brandlew.co>
2022-05-09 16:11:36 -0700

    test new tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, Script } from 'next/document';
 
 export default class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
-                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    {/* Global Site Tag (gtag.js) - Google Analytics
                     <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
                     <script
                         dangerouslySetInnerHTML={{
@@ -18,11 +18,25 @@ export default class MyDocument extends Document {
             });
           `
                         }}
-                    />
+                    /> */}
                 </Head>
+                <Script id="google-tag-manager" strategy="afterInteractive">
+                {`
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-M3RVP3L');
+                `}
+                </Script>
                 <body>
                     <Main />
                     <NextScript />
+                    <noscript
+    dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M3RVP3L" height="0" width="0" style="display: none; visibility: hidden;" />`,
+    }}
+  />
                 </body>
             </Html>
         );
