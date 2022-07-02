a7914d7da843574c4f606afcaa2028078a88cced
brandlew <hello@brandlew.co>
2022-07-02 12:59:35 -0700

    id issue fix

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -171,7 +171,7 @@ function listOfLinks(links, inMobileMenu = false) {
 
                 {linkChild.length == 0 && (
                     <li key={index} data-key={index} className={classNames('relative', 'block')}>
-                        <Action {...link} className={classNames(headerStyles, "w-full justify-start border-b-2 lg:border-0 border-solid border-secondary ", inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
+                        <Action {...link} elementId={inMobileMenu ? link.elementId + '-mobile' : link.elementId} className={classNames(headerStyles, "w-full justify-start border-b-2 lg:border-0 border-solid border-secondary ", inMobileMenu && link.type === 'Button' ? 'w-full' : '')} data-sb-field-path={`.${index}`} />
                     </li>
                 )}
             </>
