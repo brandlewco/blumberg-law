28bd343baef0d18837142d426759f636b976cfd0
brandlew <hello@brandlew.co>
2022-07-25 17:41:29 -0700

    post hero

--- src/components/layouts/PostLayout/index.tsx
+++ src/components/layouts/PostLayout/index.tsx
@@ -21,20 +21,20 @@ export default function PostLayout(props) {
         <BaseLayout page={page} site={site}>
             <main id="main" className="sb-layout sb-post-layout">
                 <article className={classNames(colors)}>
-                <header className="colors-b flex flex-col justify-center relative pt-8 pb-8 pr-4 pl-4 border-none rounded-none hero-bg">
-                        {page.media && <BackgroundImage {...page.media} />}
-                        <div className="relative z-30 max-w-7xl mx-auto py-8 md:py-20 xl:py-40 sm:px-8">
-                            {page.title && <h1 data-sb-field-path="title">{page.title}</h1>}
-                            <div className="text-lg mt-6">
-                                <span>
-                                    <time dateTime={dateTimeAttr} data-sb-field-path="date">
-                                        {formattedDate}
-                                    </time>
-                                </span>
-                                <PostAttribution post={page} />
+                    <header className="colors-b flex flex-col justify-center relative pt-8 pb-8 pr-4 pl-4 border-none rounded-none hero-bg">
+                            {page.media && <BackgroundImage {...page.media} />}
+                            <div className="relative z-30 max-w-7xl mx-auto py-8 md:py-20 xl:py-40 pl-8 pr-8 lg:pr-64">
+                                {page.title && <h1 data-sb-field-path="title">{page.title}</h1>}
+                                <div className="text-lg mt-6">
+                                    <span>
+                                        <time dateTime={dateTimeAttr} data-sb-field-path="date">
+                                            {formattedDate}
+                                        </time>
+                                    </span>
+                                    <PostAttribution post={page} />
+                                </div>
                             </div>
-                        </div>
-                        </header>
+                    </header>
                     <div className="max-w-7xl mx-auto px-4 sm:px-8 py-14 lg:py-12">
                         {/* {page.media && <div className="w-full">{postMedia(page.media)}</div>} */}
                         {page.markdown_content && (
