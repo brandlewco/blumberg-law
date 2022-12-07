d176e0eaa854eed8be31bf7d7fac94ccae302254
Stackbit Code Editor <projects@stackbit.com>
2022-12-07 03:15:35 +0000

    index.tsx: changed

--- src/components/sections/PostFeedSection/index.tsx
+++ src/components/sections/PostFeedSection/index.tsx
@@ -293,7 +293,7 @@ function postsVariantC(props) {
 
 function postsVariantD(props) {
     const dateTimeAttr = dayjs(props.date).format('YYYY-MM-DD HH:mm:ss');
-    const formattedDate = dayjs(props.date).format('MMMM D, YYYY');
+    const formattedDate = dayjs(post.date).format('MMMM D, YYYY');
     const posts = props.posts || [];
     if (posts.length === 0) {
         return null;
