2d9da1aa4addb4eb63ff88c60c96d65ac75e2172
Stackbit Code Editor <projects@stackbit.com>
2023-01-31 00:37:04 +0000

    index.tsx: changed

--- src/components/sections/CasesFeedSection/index.tsx
+++ src/components/sections/CasesFeedSection/index.tsx
@@ -185,7 +185,7 @@ function postsVariantB(props, emblaRef, emblaApi) {
     }, [emblaApi])
     return (
         <>
-            <div>{postLength}</div>
+            {/* <div>{postLength}</div> */}
             <div
                 className={classNames('embla', postLength, {
                     'mt-12': props.title || props.subtitle || (props.actions || []).length > 0
