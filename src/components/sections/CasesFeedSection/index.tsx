f126af911a232642304bea555bdc7677972e7e9d
Stackbit Code Editor <projects@stackbit.com>
2023-01-31 00:24:07 +0000

    index.tsx: changed

--- src/components/sections/CasesFeedSection/index.tsx
+++ src/components/sections/CasesFeedSection/index.tsx
@@ -176,7 +176,7 @@ function postsVariantB(props, emblaRef, emblaApi) {
     if (posts.length === 0) {
         return null;
     }
-    if (postCount =>)
+    if (postCount => )
     const scrollPrev = useCallback(() => {
         if (emblaApi) emblaApi.scrollPrev()
     }, [emblaApi])
