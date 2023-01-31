0bd5eac8240fe2549002eeab05b93b9af714b2a3
Stackbit Code Editor <projects@stackbit.com>
2023-01-31 00:21:17 +0000

    index.tsx: changed

--- src/components/sections/CasesFeedSection/index.tsx
+++ src/components/sections/CasesFeedSection/index.tsx
@@ -185,7 +185,7 @@ function postsVariantB(props, emblaRef, emblaApi) {
     return (
         <>
             <div
-                className={classNames('embla', '' {
+                className={classNames('embla',  {
                     'mt-12': props.title || props.subtitle || (props.actions || []).length > 0
                 })}
                 {...(props.annotatePosts ? { 'data-sb-field-path': '.posts' } : null)}
