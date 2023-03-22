c3da59354259c2a8879e5eb764ca44c29b3d2967
brandlew <hello@brandlew.co>
2023-03-21 21:12:12 -0700

    add local buisness schema

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -25,6 +25,42 @@ export default function DefaultBaseLayout(props) {
                       }
                       return  <meta key={metaTag.property} name={metaTag.property} content={metaTag.content} />
                     })}
+                    <script type='application/ld+json' dangerouslySetInnerHTML={ { __html: `{
+                            "@context": "https://schema.org",
+                            "@type": "Attorney",
+                            "name": "Blumberg & Associates",
+                            "address": {
+                                "@type": "PostalAddress",
+                                "streetAddress": "3600 N. 19TH Ave.",
+                                "addressLocality": "Phoenix",
+                                "addressRegion": "AZ",
+                                "postalCode": "85015"
+                            },
+                            "image": "https://azblumberglaw.com/_next/image?url=%2Fimages%2Fblumberg-hero-mockup.jpg&w=1080&q=75",
+                            "telePhone": "602-277-6180",
+                            "faxNumber": "602-889-9167",
+                            "url": "https://azblumberglaw.com/",
+                            "openingHours": "Mo,Tu,We,Th,Fr 09:00-17:00",
+                            "openingHoursSpecification": [ {
+                                "@type": "OpeningHoursSpecification",
+                                "dayOfWeek": [
+                                    "Monday",
+                                    "Tuesday",
+                                    "Wednesday",
+                                    "Thursday",
+                                    "Friday"
+                                ],
+                                "opens": "09:00",
+                                "closes": "17:00"
+                            } ],
+                            "geo": {
+                                "@type": "GeoCoordinates",
+                                "latitude": "33.489876",
+                                "longitude": "-112.100171"
+                            },
+                            "priceRange":"$"
+
+                        }`}} />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     {site.favicon && <link rel="icon" href={site.favicon} />}
                     <link rel="stylesheet" href="https://use.typekit.net/yxh4eie.css"></link>
