732fb11d7573c3d9cf6567330cd6b2b74629a387
brandlew <hello@brandlew.co>
2023-03-21 20:45:35 -0700

    add native lazy loading

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -22,6 +22,7 @@ export default function ImageBlock(props) {
             className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', cssClasses)}
             src={url}
             alt={altText || ''}
+            loading="lazy"
             data-sb-field-path={annotations.join(' ').trim()}
         />
     );
